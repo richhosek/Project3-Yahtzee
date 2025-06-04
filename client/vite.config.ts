02e7401d9f957be119a8824a3bcc3d82e313aa43
Rich Hosek <richhosek@gmail.com>
2025-06-04 16:30:52 -0500

    Revert

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     open: true,
     proxy: {
     '/graphql': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:10000',
         changeOrigin: true,
         secure: false,
       },
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,11 +6,7 @@ import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/serve
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
-import { fileURLToPath } from 'node:url';
 
-// Define __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
@@ -33,13 +29,13 @@ const startApolloServer = async () => {
     }
   ));
 
-//   if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
       res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
-//   }
+  }
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log(`API server running on port ${PORT}!`);
